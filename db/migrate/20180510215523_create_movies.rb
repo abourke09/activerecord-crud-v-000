c02b7ebb7e8d072337041e449572cab7e879fd2e
Annie Bourke <aabourke@gmail.com>
2018-05-10 21:58:07 +0000

    automatically backed up by learn

--- db/migrate/20180510215523_create_movies.rb
+++ db/migrate/20180510215523_create_movies.rb
@@ -1,4 +1,13 @@
-class CreateMovies < ActiveRecord::Migration
+class CreateMovies < ActiveRecord::Migration[5.1]
   def change
+    create_table :movies do |t|
+      t.string :title
+      t.timedate :release_date
+      t.string :director
+      t.string :lead_actor
+      t.boolean :in_theaters
+
+      t.timestamps
+    end
   end
 end
