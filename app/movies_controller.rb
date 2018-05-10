719a607e79acc726a9d1af55ff8cd8d387678c4b
Annie Bourke <aabourke@gmail.com>
2018-05-10 23:19:45 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -40,7 +40,8 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.size
+  movie = Movie.new
+  movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
